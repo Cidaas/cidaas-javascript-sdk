751911a80ba7742e8467e8a120466a60a50d11b3
Marvin Siegert <marvin.siegert@widas.de>
2021-03-23 13:31:16 +0100

    remove gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var webpackStream = require('webpack-stream');
-var webpackConfig = require('./webpack.config.js');
-
-gulp.task('build', function () {
-  return gulp.src('src/main/index.js')
-    .pipe(webpackStream(webpackConfig))
-    .pipe(gulp.dest('build/'));
-});
