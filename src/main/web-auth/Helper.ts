7e344d24f59b41737923a7536ce26bdcfe07e82e
Alvin Chandra <alvin.chandra@widas.de>
2023-10-10 08:40:50 +0200

    reset change for other issue

--- src/main/web-auth/Helper.ts
+++ src/main/web-auth/Helper.ts
@@ -37,11 +37,7 @@ export class Helper {
         http.onreadystatechange = function () {
           if (http.readyState == 4) {
             if (http.responseText) {
-              try {
-                resolve(JSON.parse(http.responseText));
-              } catch (e) {
-                resolve(http.responseText);
-              }
+              resolve(JSON.parse(http.responseText));
             } else {
               resolve(errorResolver);
             }
