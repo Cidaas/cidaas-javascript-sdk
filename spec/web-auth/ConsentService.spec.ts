cf001e43d85ac3fcb01c78364ba5a0606d03e88d
Alvin Chandra <alvin.chandra@widas.de>
2023-11-06 14:42:13 +0100

    formating indentation

--- spec/web-auth/ConsentService.spec.ts
+++ spec/web-auth/ConsentService.spec.ts
@@ -9,7 +9,7 @@ const serviceBaseUrlV1: string = `${authority}/consent-management-srv/consent`;
 const httpSpy = jest.spyOn(Helper, 'createPostPromise');
 
 beforeAll(() => {
-    (window as any).webAuthSettings = {authority: authority}
+  (window as any).webAuthSettings = { authority: authority }
 });
 
 test('getConsentDetails', () => {
@@ -24,69 +24,69 @@ test('getConsentDetails', () => {
 });
 
 test('acceptConsent', () => {
-    const option: IConsentAcceptEntity = {
-        client_id: 'client_id',
-        consent_id: 'consent_id',
-        consent_version_id: 'consent_id',
-        sub: 'sub',
-        scopes: ['scopes'],
-        url: 'url',
-        matcher: null,
-        field_key: 'field_key',
-        accepted_fields: ['accepted_fields'],
-        accepted_by: 'accepted_by',
-        skipped: true,
-        action_type: 'action_type',
-        action_id: 'action_id',
-        q: 'q',
-        revoked: false
-    };
-    const serviceURL = `${serviceBaseUrl}/usage/accept`;
-    ConsentService.acceptConsent(option);
-    expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST');
+  const option: IConsentAcceptEntity = {
+    client_id: 'client_id',
+    consent_id: 'consent_id',
+    consent_version_id: 'consent_id',
+    sub: 'sub',
+    scopes: ['scopes'],
+    url: 'url',
+    matcher: null,
+    field_key: 'field_key',
+    accepted_fields: ['accepted_fields'],
+    accepted_by: 'accepted_by',
+    skipped: true,
+    action_type: 'action_type',
+    action_id: 'action_id',
+    q: 'q',
+    revoked: false
+  };
+  const serviceURL = `${serviceBaseUrl}/usage/accept`;
+  ConsentService.acceptConsent(option);
+  expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST');
 });
 
 test('getScopeConsentVersionDetails', () => {
-    const option = {
-        scopeid: 'scopeid',
-        locale: 'locale',
-        access_token: 'access_token'
-    };
-    const serviceURL = `${serviceBaseUrl}/versions/details/${option.scopeid}?locale=${option.locale}`;
-    ConsentService.getScopeConsentVersionDetails(option);
-    expect(httpSpy).toHaveBeenCalledWith(undefined, serviceURL, false, 'GET', option.access_token);
+  const option = {
+    scopeid: 'scopeid',
+    locale: 'locale',
+    access_token: 'access_token'
+  };
+  const serviceURL = `${serviceBaseUrl}/versions/details/${option.scopeid}?locale=${option.locale}`;
+  ConsentService.getScopeConsentVersionDetails(option);
+  expect(httpSpy).toHaveBeenCalledWith(undefined, serviceURL, false, 'GET', option.access_token);
 });
 
 test('acceptScopeConsent', () => {
-    const option = {
-        client_id: 'client_id',
-        sub: 'sub',
-        scopes: ['scopes']
-    };
-    const serviceURL = `${serviceBaseUrlV1}/scope/accept`;
-    ConsentService.acceptScopeConsent(option);
-    expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST');
+  const option = {
+    client_id: 'client_id',
+    sub: 'sub',
+    scopes: ['scopes']
+  };
+  const serviceURL = `${serviceBaseUrlV1}/scope/accept`;
+  ConsentService.acceptScopeConsent(option);
+  expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST');
 });
 
 test('acceptClaimConsent', () => {
-    const option = {
-        client_id: 'client_id',
-        sub: 'sub',
-        accepted_claims: ['accepted_claims']
-    };
-    const serviceURL = `${serviceBaseUrlV1}/claim/accept`;
-    ConsentService.acceptClaimConsent(option);
-    expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false,  'POST');
+  const option = {
+    client_id: 'client_id',
+    sub: 'sub',
+    accepted_claims: ['accepted_claims']
+  };
+  const serviceURL = `${serviceBaseUrlV1}/claim/accept`;
+  ConsentService.acceptClaimConsent(option);
+  expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST');
 });
 
 test('revokeClaimConsent', () => {
-    const option = {
-        access_token: 'access_token',
-        client_id: 'client_id',
-        sub: 'sub',
-        revoked_claims: ['revoked_claims']
-    };
-    const serviceURL = `${serviceBaseUrlV1}/claim/revoke`;
-    ConsentService.revokeClaimConsent(option);
-    expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST', option.access_token);
+  const option = {
+    access_token: 'access_token',
+    client_id: 'client_id',
+    sub: 'sub',
+    revoked_claims: ['revoked_claims']
+  };
+  const serviceURL = `${serviceBaseUrlV1}/claim/revoke`;
+  ConsentService.revokeClaimConsent(option);
+  expect(httpSpy).toHaveBeenCalledWith(option, serviceURL, false, 'POST', option.access_token);
 });
