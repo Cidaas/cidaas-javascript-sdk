da67464e2207e8fbb41e84e08dc225596b6a4c83
Alvin Chandra <alvin.chandra@widas.de>
2024-12-10 10:03:35 +0100

    update invokeIdValidationCase() function

--- src/main/id-validation-service/IdValidationService.ts
+++ src/main/id-validation-service/IdValidationService.ts
@@ -3,7 +3,8 @@ import { InvokeIdValidationCaseRequest } from "./IdValidationService.model";
 
 /**
   * To invoke a new id validation case, call invokeIdValidationCase(). 
-  * Make sure that the access token, which is used to call this api (either as parameter or from user storage) have the following scope: cidaas:idval_init
+  * Make sure that the access token, which is used to call this api (either as parameter or from user storage) 
+  * have the following scopes: cidaas:idval_init cidaas:idval_perform cidaas:idval_settings_read profile
   *
   * @param options 
   * @param access_token
@@ -26,9 +27,15 @@ import { InvokeIdValidationCaseRequest } from "./IdValidationService.model";
 export function invokeIdValidationCase(options: InvokeIdValidationCaseRequest, access_token?: string) {
     const serviceURL = window.webAuthSettings.authority + '/idval-sign-srv/caseinvocation';
     if (access_token) {
-        return Helper.createHttpPromise(options, serviceURL, false, 'POST', access_token);
+        return Helper.createHttpPromise(options, serviceURL, false, 'POST', access_token).then(response => {
+            const redirectUrl = response.data.case_redirect_url;
+            window.location.href = redirectUrl;
+        });
     }
     return Helper.getAccessTokenFromUserStorage().then((accessToken) => {
-        return Helper.createHttpPromise(options, serviceURL, false, 'POST', accessToken);
+        return Helper.createHttpPromise(options, serviceURL, false, 'POST', accessToken).then(response => {
+            const redirectUrl = response.data.case_redirect_url;
+            window.location.href = redirectUrl;
+        });
     });
 }
