ae8ce3e029571f9982b8e24ffc7e383abb2b23ec
Alvin Chandra <alvin.chandra@widas.de>
2024-12-16 08:24:46 +0100

    remove oidcClient from global

--- src/main/global.d.ts
+++ src/main/global.d.ts
@@ -1,12 +1,5 @@
-import { OidcClient } from "oidc-client-ts";
-
 declare global {
   interface Window {
-    /**
-     * OpenId client
-     */
-    oidcClient: OidcClient;
-
     /**
      * Locale configured during init
      */
@@ -14,3 +7,4 @@ declare global {
   }
 }
 
+export {}
